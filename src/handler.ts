bf6bcecfbece1451ce06412c7fd25fdcd97a3231
sunrabbit123 <qudwls185@naver.com>
2021-11-06 21:46:31 +0900

    Add : declare function

---
+++ src/handler.ts
@@ -0,0 +1,14 @@
+export const getAlgorithemCountAtAll: Function = undefined;
+export const getAlgorithemList: Function = undefined;
+export const getAlgorithemListAtPages: Function = undefined;
+export const wirteAlogorithem: Function = undefined;
+export const setAlogorithemStatus: Function = undefined;
+export const modifyAlogirithemContent: Function = undefined;
+export const reportAlogorithem: Function = undefined;
+export const deleteAlgorithem: Function = undefined;
+export const authAdmin: Function = undefined;
+export const getVerifyQuestion: Function = undefined;
+export const login: Function = undefined;
+export const logOut: Function = undefined;
+export const addLeaf: Function = undefined;
+export const removeLeaf: Function = undefined;
