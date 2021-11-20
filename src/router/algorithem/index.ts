19ebe0b74a634447a4fcfbb14e04633ec7acbc5b
sunrabbit123 <qudwls185@naver.com>
2021-11-20 13:53:00 +0900

    update : get rule functions

--- src/router/algorithem/index.ts
+++ src/router/algorithem/index.ts
@@ -1,5 +1,7 @@
 export class AlgorithemRouter {
   static async getAlgorithemCountAtAll() {}
+  static async getAlgorithemRules(){}
+  static async getAlgorithemRulesForWeb(){}
   static async getAlgorithemList() {}
   static async getAlgorithemListAtPages() {}
   static async wirteAlogorithem() {}
