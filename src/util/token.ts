a0a2e2d13efe14a689f5b47e246442520c6ab8d5
sunrabbit123 <qudwls185@naver.com>
2021-12-27 14:44:46 +0900

    add : generateAccessToken, generate RefreshToken

--- src/util/token.ts
+++ src/util/token.ts
@@ -1,5 +1,8 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 
+import { issuer } from "../config";
+import { AccessTokenArgumentDTO } from "../DTO/user.dto";
+
 export const verifyToken = (token: string): null | string | JwtPayload => {
   try {
     return jwt.verify(token, process.env.JWT_SECRET);
@@ -7,3 +10,19 @@ export const verifyToken = (token: string): null | string | JwtPayload => {
     return null;
   }
 };
+
+export const generateAccessToken = (data: AccessTokenArgumentDTO) =>
+  jwt.sign(
+    Object.assign({}, data, { tokenType: "AccessToken" }),
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+      issuer,
+    }
+  );
+
+export const generateRefreshToken = () =>
+  jwt.sign({ tokenType: "AccessToken" }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+    issuer,
+  });
