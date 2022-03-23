4de023b2c6b571a1701d2db309a6f63e08105e7a
sunrabbit123 <qudwls185@naver.com>
2022-03-23 18:17:01 +0900

    add : delete test codes

---
+++ test/delete.test.ts
@@ -0,0 +1,43 @@
+import "reflect-metadata";
+
+import { ADMIN_JWT } from "./config";
+import { baseRequest } from "./dummy.data";
+
+import { deleteAlgorithm, getAlgorithmListByUser } from "../src/handler";
+
+describe("delete algorithm", () => {
+  const cursorReq = {
+    ...{ ...baseRequest, method: "GET" },
+    pathParameters: { type: "cursor" },
+  };
+
+  const getDeleteReq = (id: number) => ({
+    ...baseRequest,
+    pathParameters: { id },
+  });
+
+  const getAlgorithm = async () => {
+    return JSON.parse(
+      (
+        await getAlgorithmListByUser({
+          ...cursorReq,
+        })
+      ).body,
+    ).data.data[0];
+  };
+
+  test("wiil be success", async () => {
+    const targetAlgorithm = await getAlgorithm();
+
+    const result = await deleteAlgorithm({
+      ...getDeleteReq(targetAlgorithm.idx),
+      headers: { authorization: ADMIN_JWT },
+    });
+
+    expect(JSON.parse(result.body)).toEqual(
+      expect.objectContaining({
+        code: "JL000",
+      }),
+    );
+  });
+});
