5e994c09371c94b4d9f77986c736e2faa12b644f
sunrabbit123 <qudwls185@naver.com>
2022-05-06 23:34:12 +0900

    refact: retyping functions

--- src/handler.ts
+++ src/handler.ts
@@ -1,40 +1,56 @@
 import {
-  AlgorithmRouter,
-  EmojiRouter,
-  AuthRouter,
-  CommentRouter,
-  ProfileRouter,
-} from "./router";
+  getAlgorithmCountAll,
+  getAlgorithmRule,
+  getAlgorithmRuleForWeb,
+  getAlgorithmByUser,
+  getAlgorithmListByUser,
+  getAlgorithmListByAdmin,
+  insertAlgorithm,
+  modifyAlgorithmContents,
+  modifyAlgorithmStatus,
+  removeAlgorithm,
+} from "./router/algorithm";
 
-export const getAlgorithmCountAtAll: Function =
-  AlgorithmRouter.getAlgorithmCountAtAll;
-export const getAlgorithmRules: Function = AlgorithmRouter.getAlgorithmRules;
-export const getAlgorithmRulesForWeb: Function =
-  AlgorithmRouter.getAlgorithmRulesForWeb;
-export const getAlgorithmByUser: Function = AlgorithmRouter.getAlgorithmByUser;
-export const getAlgorithmListByUser: Function =
-  AlgorithmRouter.getAlgorithmListByUser;
-export const getAlgorithmListByAdmin: Function =
-  AlgorithmRouter.getAlgorithmListByAdmin;
-export const wirteAlgorithm: Function = AlgorithmRouter.wirteAlgorithm;
-export const setAlgorithmStatus: Function = AlgorithmRouter.setAlgorithmStatus;
-export const modifyAlogirithemContent: Function =
-  AlgorithmRouter.modifyAlgorithmContent;
-export const deleteAlgorithm: Function = AlgorithmRouter.deleteAlgorithm;
+import {
+  addVeirfyQuestions,
+  getVerifyQuestions,
+  login,
+  mailAuth,
+  mailSend,
+  refreshTokens,
+  regist,
+} from "./router/auth";
 
-export const getVerifyQuestion: Function = AuthRouter.getVerifyQuestion;
-export const addVerifyQuestion: Function = AuthRouter.addVerifyQuestion;
-export const getTokenByRefreshToken: Function =
-  AuthRouter.getTokenByRefreshToken;
-export const login: Function = AuthRouter.login;
-export const signUp: Function = AuthRouter.signUp;
-export const mailSend: Function = AuthRouter.mailSend;
-export const mailAuth: Function = AuthRouter.mailAuth;
+import { addComments, removeComment } from "./router/comment";
 
-export const addLeaf: Function = EmojiRouter.addLeaf;
-export const removeLeaf: Function = EmojiRouter.removeLeaf;
+import { leafAdd, leafRemove } from "./router/emoji";
 
-export const addComment: Function = CommentRouter.addComment;
-export const deleteComment: Function = CommentRouter.deleteComment;
+import { profileModify } from "./router/profile";
+export {
+  getAlgorithmCountAll as getAlgorithmCountAtAll,
+  getAlgorithmRule as getAlgorithmRules,
+  getAlgorithmRuleForWeb as getAlgorithmRulesForWeb,
+  getAlgorithmByUser,
+  getAlgorithmListByUser,
+  getAlgorithmListByAdmin,
+  insertAlgorithm as writeAlgorithm,
+  modifyAlgorithmContents as modifyAlgorithmContent,
+  modifyAlgorithmStatus as setAlgorithmStatus,
+  removeAlgorithm as deleteAlgorithm,
+};
 
-export const modifyProfile: Function = ProfileRouter.modifyProfile;
+export {
+  addVeirfyQuestions as addVerifyQuestion,
+  getVerifyQuestions as getVerifyQuestion,
+  login,
+  mailAuth,
+  mailSend,
+  refreshTokens as getTokenByRefreshToken,
+  regist as signUp,
+};
+
+export { addComments as addComment, removeComment as deleteComment };
+
+export { leafAdd as addLeaf, leafRemove as removeLeaf };
+
+export { profileModify as modifyProfile };
